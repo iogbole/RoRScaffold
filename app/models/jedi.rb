24fbd694937e0ac7abae0a067887a7a484b684e6
Israel Ogbole <israel.ogbole@microsoft.com>
2014-07-10 10:42:50 +0100

    Re-form

--- app/models/jedi.rb
+++ app/models/jedi.rb
@@ -1,3 +1,10 @@
 class Jedi < ActiveRecord::Base
 	mount_uploader :picture, PictureUploader
+  
+  before :save, :set_location
+
+  def set_location
+    location = "#{street_address} #{street_address_2} #{county} #{postal_code} #{country}"
+  end
+
 end
